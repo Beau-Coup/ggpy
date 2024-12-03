ba29eceba61dd611e50e3363ea60b37203460aa7
ab <48172047+Beau-Coup@users.noreply.github.com>
2024-12-02 20:46:45 -0800

    minors shape

--- src/ggpy/__about__.py
+++ src/ggpy/__about__.py
@@ -1,4 +1,4 @@
 # SPDX-FileCopyrightText: 2024-present ab <48172047+Beau-Coup@users.noreply.github.com>
 #
 # SPDX-License-Identifier: MIT
-__version__ = "0.2.6"
+__version__ = "0.2.7"
--- src/ggpy/gp.py
+++ src/ggpy/gp.py
@@ -47,8 +47,7 @@ class GP:
                 )
                 self.k[old_size:, :old_size] = self.k[:old_size, old_size:].T
                 self.k[old_size:, old_size:] = (
-                    self.kernel.eval(new_x)
-                    + np.identity(self.train_inputs.shape[0]) * 1e-10
+                    self.kernel.eval(new_x) + np.identity(new_x.shape[0]) * 1e-10
                 )
 
                 old_cho = self.cho.copy()
